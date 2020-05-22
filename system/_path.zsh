ae1b8f286aba7ef57d90ed2b056ee970df4241cd
Aravinth Panch <me@aravinth.info>
2020-05-22 15:34:52 +0200

    Add npm global install to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSHDOT/bin:$PATH"
 export PATH="~/sftw:$PATH"
+export PATH="/usr/local/Cellar/node/13.5.0/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
