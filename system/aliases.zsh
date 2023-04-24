492d5763bdbf8edb1af667f2df2fea7e0d6f19c8
Aravinth Panch <me@aravinth.info>
2023-04-24 11:27:09 +0200

    feat: update python and ruby aliases and path

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -22,4 +22,13 @@ alias esp32_env_setup='. $HOME/esp/esp-idf/export.sh'
 alias git-modified='npanelize git ls-files -m'
 
 # default python is python3
-alias python=python3
+#alias python=/opt/homebrew/bin/python3
+#alias python3=python
+#alias pip=/opt/homebrew/bin/pip3
+#alias pip3=pip
+alias pip=/usr/local/bin/pip3
+alias python=/usr/local/bin/python3
+
+#ruby
+alias bundle=/opt/homebrew/lib/ruby/gems/3.2.0/bin/bundle
+alias bundler=/opt/homebrew/lib/ruby/gems/3.2.0/bin/bundler
