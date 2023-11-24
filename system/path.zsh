fe25c83cd5b5f0e0330f4c48fe350afb716527bc
Aravinth Panch <ara@aracreate.com>
2023-11-24 13:01:16 +0100

    chore: update arametrics bin, add zephyr, android to paths

--- system/path.zsh
+++ system/path.zsh
@@ -5,11 +5,18 @@ export PATH="$PATH:/Applications/flutter/bin/"
 export PATH="$PATH:/opt/homebrew/bin"
 export PATH="$PATH:/opt/homebrew/opt/openjdk/bin"
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
+export PATH="$PATH:/Users/aravinth/Library/Android/sdk/platform-tools"
+
+#python
 #export PYTHONPATH="$PYTHONPATH:/opt/homebrew/lib/python3.9/site-packages"
 export PYTHONPATH="$PYTHONPATH:/opt/homebrew/lib/python3.11/site-packages"
+
 #ruby
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 export PATH="/opt/homebrew/lib/ruby/gems/3.2.0/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/ruby/lib/pkgconfig"
+
+#zephyy
+export ZEPHYR_BASE="/Users/aravinth/.zephyrproject/zephyr"
